19bbb694d98b9b78e6406d6f5dc9f76139384a29
Kinha220 <96486168+Kinha220@users.noreply.github.com>
2026-04-28 22:36:49 -0300

    Adjust X position for 'Provisória' condition

--- app.py
+++ app.py
@@ -112,9 +112,9 @@ def marcar_carater_visual(writer, carater):
     # Primeiro número = esquerda/direita
     # Segundo número = sobe/desce
     if carater == "Provisória":
-        c.drawString(300, 277, "X")
+        c.drawString(280, 276, "X")
     else:
-        c.drawString(420, 277, "X")
+        c.drawString(420, 276, "X")
 
     c.save()
     packet.seek(0)
