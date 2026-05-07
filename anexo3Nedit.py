db288a69698370dbca34b1d2da40a7cce8b82d70
Kinha220 <96486168+Kinha220@users.noreply.github.com>
2026-05-07 18:29:22 +0000

    para homologação

--- anexo3Nedit.py
+++ anexo3Nedit.py
@@ -6,7 +6,7 @@ from reportlab.lib.pagesizes import A4
 import textwrap
 
 st.set_page_config(page_title="Anexo III - PDF Fixo", layout="centered")
-st.title("📄 Gerador Anexo III - PDF não editável")
+st.title("📄 Gerador de PDF do Anexo III")
 
 # =========================
 # FORMULÁRIO
